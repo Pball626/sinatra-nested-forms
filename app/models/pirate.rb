74e07a356e0ae4654dc020d6c8f7ece927dd6955
joey <pball626@gmail.com>
2021-03-16 17:36:16 -0500

    commit tree fb288e612918065ae9bf23764d093191f9116bb4

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+    @@all = []
+    def initalize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+        @@all << self
+
+    end
+
+    def self.all
+        @@all
+    end
+        
 end
